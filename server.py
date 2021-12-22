12d5c6905c98e696c8b2b16c64a3d14a30b9817c
gcd0318 <gcd0318@hotmail.com>
2021-12-22 13:44:12 +0800

    port and non-ascii with weather json

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from weather import get_weather
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def index():
     return 'mirror'
 
+@app.route('/weather')
+def weather():
+    name, data = get_weather()
+    return jsonify({name: data})
+
+
 if '__main__' == __name__:
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8888, debug=True)
