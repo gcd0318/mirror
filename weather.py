cd6cf8a83ba22e18373a6e8be07b832fa98e03f9
gcd0318 <gcd0318@hotmail.com>
2021-12-22 13:45:02 +0800

    weather hold None

--- weather.py
+++ weather.py
@@ -2,7 +2,12 @@ from base import chain_get
 
 def get_weather(conffile='weather.conf'):
     sect, data = chain_get(conffile)
-    print(data)
+    name = None
+    if sect is not None:
+        name = sect.get('name')
+    if data is not None:
+        pass
+    return name, data
 
 if '__main__' == __name__:
     print(get_weather())
