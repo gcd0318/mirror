6870f90d1c35bc8de2eb8f4f66cb5960604173ff
gcd0318 <gcd0318@hotmail.com>
2021-12-23 15:26:51 +0800

    use config in etc

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 from flask import Flask, jsonify, render_template
 from weather import get_weather
 
+import os
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def index():
-    data = get_weather()
+    data = get_weather('/etc/mirror/weather.conf')
     return render_template('index.html', data=data)
 
 @app.route('/weather')
 def weather():
-    data = get_weather()
+    data = get_weather('/etc/mirror/weather.conf')
     return jsonify(data)
 
 
