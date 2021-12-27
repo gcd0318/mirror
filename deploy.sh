c90b481d9a91d46e924a96c891e4e89ccc921e87
gcd0318 <gcd0318@hotmail.com>
2021-12-27 11:50:22 +0800

    cp autostart to ~/.config

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ cp index.mirror.html /var/www/html
 
 mkdir /var/log/mirror
 
+cp -r autostart ~/.config
+
 cp mirror.service /usr/lib/systemd/system
 systemctl reenable mirror
 systemctl restart mirror.service
